ae49af1de7872c0cbe222abf3be899c6768d5d3d
Perry Wang <perrywangsh@gmail.com>
2016-03-08 16:18:08 +0800

    change slider code to avoid 3 images display same time full screen

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,12 +1,15 @@
-$('#home-slider').lightSlider(
-        {
-            item: 1,
-            mode: 'fade',
-            auto: true,
-            loop: true,
-            controls: true,
-            pause: 4000,
-            speed: 800,
+$(function(){
+  $('.home-slide').css('visibility','visible');
+  $('#home-slider').lightSlider(
+          {
+              item: 1,
+              mode: 'fade',
+              auto: true,
+              loop: true,
+              controls: true,
+              pause: 4000,
+              speed: 800,
 
-        }
-);
+          }
+  );
+});
